74d568f0687f726a46e836812b1932084395692d
Jorge Ortiz <jorge@neurealities.com>
2025-07-17 16:31:29 -0500

    fix(flask): Remove debug flag to avoid conflicting watchdogs

--- watchdog/camera.py
+++ watchdog/camera.py
@@ -1,6 +1,10 @@
+import os
 import cv2
 
 class VideoCamera(object):
+
+    videos = [] # Implemented as a queue
+
     def __init__(self):
         # Using OpenCV to capture from device 0. If you have trouble capturing
         # from a webcam, comment the line below out and use a video file
@@ -14,9 +18,20 @@ class VideoCamera(object):
         self.video.release()
     
     def get_frame(self):
+        """Use opencv get get frame image"""
         success, image = self.video.read()
         # We are using Motion JPEG, but OpenCV defaults to capture raw images,
         # so we must encode it into JPEG in order to correctly display the
         # video stream.
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
+    
+    def add_video(self, path: os.PathLike):
+        """Add videos from watchdog"""
+        self.videos.append(path)
+        print(f"Video [{self.videos.count}] added: {path}")
+
+    def clear_videos(self):
+        """Clear video render queue"""
+        print("Cleared videos")
+        self.videos = []
--- watchdog/watchdog_app.py
+++ watchdog/watchdog_app.py
@@ -15,9 +15,10 @@ from watchdog.events import FileSystemEventHandler
 
 VOICE_DATA_PATH = os.path.expanduser(r"~/heygem_data/voice/data")
 VIDEO_TEMP_PATH = os.path.expanduser(r"~/heygem_data/face2face/temp")
-autoPlay  = False
-isPlaying = True
 
+AUTOPLAY   = False
+IS_PLAYING = True
+CAMERA     = None
 
 def rel_path(abs_path:str):
     """Returns relative path from watched directory, for easier display"""
@@ -30,23 +31,25 @@ class TempFileHandler(FileSystemEventHandler):
         rpath = rel_path(event.src_path)
         if event.is_directory:
             print("Created: Directory:", rpath)
+            pass
         else:
-            print("Created: File:", rpath)
+            #print("Created: File:", rpath)
+            pass
         return super().on_created(event)
 
     def on_modified(self, event):
         if event.is_directory:  # Ignore directory modifications if only files are desired
             return
         rpath = rel_path(event.src_path)
-        print("Modified: File:", rpath)
+        #print("Modified: File:", rpath)
         return super().on_modified(event)
 
     def on_deleted(self, event):
         rpath = rel_path(event.src_path)
         if event.is_directory:
-            print("Deleted: Directory", rpath)
+            print("Deleted: Directory:", rpath)
         else:
-            print("Deleted: File", rpath)
+            print("Deleted: File:", rpath)
         return super().on_deleted(event)
 
     def on_closed(self, event):
@@ -55,22 +58,27 @@ class TempFileHandler(FileSystemEventHandler):
         return super().on_closed(event)
 
 def handle_closed_files(rpath: str):
-    print("Closed: File", rpath)
+    """Handler when created or modified files have been closed"""
+    global CAMERA
+    print("Closed: File:", rpath)
     if rpath == "output/audio_data.npy":
         # We are ready to start receiving video files
+        print(f"audio_data.npy", CAMERA)
         return
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', audioAutoPlay = 'autoplay' if autoPlay else '')
+    """Render main flask page"""
+    return render_template('index.html', audioAutoPlay = 'autoplay' if AUTOPLAY else '')
 
 def gen(camera):
-    global autoPlay, isPlaying
-    isPlaying = autoPlay
+    """Get frames from camera class"""
+    global AUTOPLAY, IS_PLAYING
+    IS_PLAYING = AUTOPLAY # Initial state. Notice that AUTOPLAY=True might not work due to browser restriction on unwanted audio play without user intervention
     while True:
-        if isPlaying:
+        if IS_PLAYING:
             frame = camera.get_frame()
             time.sleep(1.0 / 25.0)
             yield (b'--frame\r\n'
@@ -78,18 +86,24 @@ def gen(camera):
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(VideoCamera()),
+    """Get camera object from cv2.VideoCapture"""
+    global CAMERA
+    CAMERA = VideoCamera()
+    print(CAMERA)
+    return Response(gen(CAMERA),
         mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route("/pause", methods=['POST'])
 def pause():
-    global isPlaying
-    isPlaying = not(isPlaying)
-    print("is Playing: ", isPlaying)
-    return jsonify({'playing': isPlaying})
+    """Called from the HTML page"""
+    global IS_PLAYING
+    IS_PLAYING = not IS_PLAYING
+    print("is Playing: ", IS_PLAYING)
+    return jsonify({'playing': IS_PLAYING})
 
 @app.route("/wav")
 def wav():
+    """Get audio file and synchronize it to the images being displayed"""
     def generate():
         with open("samples/sample.wav", "rb") as fwav:
             data = fwav.read(1024)
@@ -99,9 +113,12 @@ def wav():
     return Response(generate(), mimetype="audio/x-wav")
 
 
-if __name__ == "__main__":
-    # Display page
-    app.run(host='0.0.0.0', debug=True)
+def main():
+    """Main watchdog function to observe files created by heygem-gen-video docker service"""
+    # Initialize
+    global AUTOPLAY, IS_PLAYING
+    AUTOPLAY  = False
+    IS_PLAYING = True
     # Watchdog subscribe
     path  = VIDEO_TEMP_PATH
     observer = Observer()
@@ -110,10 +127,14 @@ if __name__ == "__main__":
     observer.start()
     print("Watchdog: Observing files...")
     print("Use Ctrl+C for KeyboardInterrupt.")
-    try:
-        while True:
-            time.sleep(1) # Keep the main thread alive
-    except KeyboardInterrupt:
-        observer.stop()
+
+    # Run server
+    app.run(host='0.0.0.0', debug=False) # False because otherwise server creates another interfering watchdog instance
+
+    # End when server ends
     print("Watchdog: Finished.")
+    observer.stop()
     observer.join()
+
+if __name__ == '__main__':
+    main()
